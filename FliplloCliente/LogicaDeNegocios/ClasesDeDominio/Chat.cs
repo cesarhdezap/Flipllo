84a8b71729104244a8547b0cea6605541e1acf5b
Cesar Hernandez Aparicio <cesarhdezap@gmail.com>
2019-10-07 20:39:27 -0500

    Añadidas clases del chat y proxy de los servicios del servidor.

---
+++ FliplloCliente/LogicaDeNegocios/ClasesDeDominio/Chat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaDeNegocios.ServiciosDeChat;
+
+namespace LogicaDeNegocios.ClasesDeDominio
+{
+    public class Chat
+    {
+        public List<Usuario> UsuariosConectados { get; set; } = new List<Usuario>();
+        public List<Mensaje> MensajesRecibidos { get; set; } = new List<Mensaje>();
+
+        public string MensajesToString()
+        {
+            string cadenaDeMensajes = string.Empty;
+
+            foreach (Mensaje mensaje in MensajesRecibidos)
+            {
+                cadenaDeMensajes = cadenaDeMensajes + System.Environment.NewLine + mensaje.ToString();
+            }
+
+            return cadenaDeMensajes;
+        }
+    }
+}
---
+++ FliplloCliente/LogicaDeNegocios/Proxy/ChatDelServidorProxy.cs
@@ -0,0 +1,12 @@
+﻿using LogicaDeNegocios.ServiciosDeChat;
+using System.ServiceModel;
+
+namespace LogicaDeNegocios.Proxy
+{
+    public class ChatDelServidorProxy : DuplexClientBase<IServiciosDeChat>
+    {
+        public ChatDelServidorProxy(IServiciosDeChatCallback callback) : base (callback)
+        {
+        }
+    }
+}
